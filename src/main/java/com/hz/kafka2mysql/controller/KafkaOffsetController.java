20d3c5ad8965a6716b8dbd082e31334c62dbd078
guochuang <chuangAug@163.com>
2020-04-16 14:52:19 +0800

    调整测试接口

--- src/main/java/com/hz/kafka2mysql/controller/KafkaOffsetController.java
+++ src/main/java/com/hz/kafka2mysql/controller/KafkaOffsetController.java
@@ -1,9 +1,7 @@
 package com.hz.kafka2mysql.controller;
 
 import com.hz.kafka2mysql.domain.KafkaOffset;
-import com.hz.kafka2mysql.domain.ReportContent;
 import com.hz.kafka2mysql.repository.KafkaOffsetRepository;
-import com.hz.kafka2mysql.service.ReportContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +21,6 @@ public class KafkaOffsetController {
 
     @RequestMapping("/getKafkaOffset")
     public KafkaOffset getKafkaOffset(String topic,String groupId){
-        return  kafkaOffsetRepository.getKafkaOffsetByTopicAndAndGroupId("1","1");
+        return  kafkaOffsetRepository.getKafkaOffsetByTopicAndAndGroupIdandAndPartition("1","1",1);
     }
 }
